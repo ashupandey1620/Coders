8d7d7c0645c56aa7c97ad445c57ee769f520db50
Ashutosh Pandey <ashupandey1620@gmail.com>
2024-03-30 10:02:19 +0530

    Splash Screen initialise

--- app/src/main/java/com/ashutosh/bingo/MainActivity.kt
+++ app/src/main/java/com/ashutosh/bingo/MainActivity.kt
@@ -10,10 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ashutosh.bingo.ui.theme.BingoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //initializing the splash Screen using API
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContent {
             BingoTheme {
@@ -22,25 +27,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize() ,
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String , modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!" ,
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BingoTheme {
-        Greeting("Android")
-    }
-}
