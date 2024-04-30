1a2a010ad7e615ba78d7eae222b2d7f0294280a4
Ashutosh Pandey <ashupandey1620@gmail.com>
2024-04-30 15:13:01 +0530

    API Interface

--- app/src/main/java/com/ashutosh/bingo/ApiInterface/ApiInterface.kt
+++ app/src/main/java/com/ashutosh/bingo/ApiInterface/ApiInterface.kt
@@ -7,7 +7,8 @@ interface ApiInterface {
 //    suspend fun login(
 //        @Body refreshToken: LoginRequest
 //    ): Response<LoginResponse>
-//
+
+
 //    @Headers("Content-Type: application/json")
 //    @POST("/api/user/register/")
 //    suspend fun register(
@@ -15,6 +16,12 @@ interface ApiInterface {
 //    ):Response<RegisterResponse>
 
 
+//    @Multipart
+//    @POST("survey")
+//    fun postSurveyResponse(
+//        @PartMap() partMap: MutableMap<String,RequestBody>,
+//        @Part file: MultipartBody.Part
+//    ):Response<Unit>
 
 
 }
